7df3444cde0832a81b1aef52e5366a26cfad8348
Akinsola Olajide <akinsolaolajide150@gmail.com>
2025-05-11 00:42:50 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Hero from "@/components/home/hero";
 import AboutUs from "@/components/about/aboutUs";
 import ContactUs from "@/components/contact/contactUs";
-import RegistrationForm from "@/components/registration/registrationForm";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
     <Hero/>
     <AboutUs/>
     <ContactUs/>
-    <RegistrationForm/>
    </div>
   );
 }
